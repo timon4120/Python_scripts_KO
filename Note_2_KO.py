ae16cb32e782e1138c39a2ed013ba93bab7024a3
Kamil Orzechowski <62968263+timon4120@users.noreply.github.com>
2022-11-08 01:49:00 +0100

    Update Note_2_KO.py

--- Note_2_KO.py
+++ Note_2_KO.py
@@ -2,6 +2,7 @@ import argparse
 import numpy as np
 import random as rd
 import rich
+import copy
 import rich.progress
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -26,10 +27,10 @@ Flags:
 """
 
 class Ising():
-    def __init__(self,n,J,beta,B,steps,dens):
+    def __init__(self,n,J,T,B,steps,dens):
         self.n = n
         self.J = J
-        self.beta = beta
+        self.T = T
         self.B = B
         self.steps = steps
         self.dens = dens
@@ -38,15 +39,15 @@ class Ising():
         self.gr = []
     
     def Get_E(self,x,y,mark = 1):
-        temp_x = x
-        temp_y = y
-        mtx = self.matrix.copy()
+        temp_x = x+1
+        temp_y = y+1
+        mtx = copy.deepcopy(self.matrix)
         if x == self.n -1: temp_x = 0 
         if y == self.n -1: temp_y = 0 
         if mark != 1: mtx[x,y] *= -1 
         neig = np.array([mtx[temp_x,y],mtx[x,temp_y],mtx[x-1,y],mtx[x,y-1]])
-        neighbourhood = -self.J * np.sum(mtx[x,y]*neig)
-        field = -self.B * mtx[x,y]
+        neighbourhood = -self.J * np.sum(neig) * mtx[x,y]
+        field = -self.B * np.sum(mtx)
         return neighbourhood + field
 
     def Simulation(self,step_tit,anim_tit):
@@ -55,11 +56,11 @@ class Ising():
                 x,y = np.random.randint(self.n, size = 2)
                 E_0 = self.Get_E(x,y)
                 E_1 = self.Get_E(x,y,-1)
-                delta = E_0 - E_1
+                delta = E_1 - E_0
                 if delta < 0: 
                     self.matrix[x,y] *= -1
-                else:
-                    if rd.uniform(0,1) < np.exp(-self.beta * delta): self.matrix[x,y] *= -1
+                elif rd.uniform(0,1) < np.exp(-1/self.T * delta): 
+                    self.matrix[x,y] *= -1
             if step_tit != None: self.Print_Grid(step,np.mean(self.matrix),step_tit)
             if anim_tit != None: self.gr.append(np.flip(self.matrix.copy(),0))
             self.M[step] = np.mean(self.matrix)
@@ -71,13 +72,14 @@ class Ising():
     
     def Print_Grid(self,i,mm, title):
         plt.rcParams['font.size'] = 16
-        cMap = c.ListedColormap(['darkred','darkgreen'])
+        cMap = c.ListedColormap(['black','white'])
         plt.figure(figsize = (15,15))
         plt.pcolormesh(np.flip(self.matrix,0),cmap=cMap)
         plt.xticks(np.arange(self.n))
         plt.yticks(np.arange(self.n))
-        plt.plot([],[], color = 'darkgreen', label = r'$s_{i} = \uparrow$')
-        plt.plot([],[], color = 'darkred', label = r'$s_{i} = \downarrow$')
+        plt.plot([],[], color = 'white', label = r'$s_{i} = \uparrow$')
+        plt.plot([],[], color = 'black', label = r'$s_{i} = \downarrow$')
+        plt.axis("off")
         plt.legend(bbox_to_anchor=(1, 1.1))
         plt.title(f'Model Isinga \n Siatka: {self.n}x{self.n}  | Krok czasowy: {i} | Magnetyzacja: {round(mm,3)} \n Kamil Orzechowski')
         plt.grid(which='major', alpha=0.5)
@@ -85,8 +87,8 @@ class Ising():
         plt.close()
 
     def animate2(self,i):
-        self.cMap = c.ListedColormap(['darkred','darkgreen'])
-        plt.title(f'Model Isinga \n Siatka: {self.n}x{self.n}  | Krok czasowy: {i} \n Kamil Orzechowski')
+        self.cMap = c.ListedColormap(['black','white'])
+        plt.title(f'Model Isinga \n N: {self.n} | J: {self.J} | T: {self.T} | B: {self.B} | Dens: {self.dens} | t: {i} | M: {round(self.M[i],2)} \n Kamil Orzechowski')
         plt.pcolormesh(self.gr[i],cmap=self.cMap)
         
     def Animate_Grid(self,title):
@@ -95,8 +97,9 @@ class Ising():
         plt.rcParams['font.size'] = 16
         plt.xticks(np.arange(self.n))
         plt.yticks(np.arange(self.n))
-        plt.plot([],[], color = 'darkgreen', label = r'$s_{i} = \uparrow$')
-        plt.plot([],[], color = 'darkred', label = r'$s_{i} = \downarrow$')
+        plt.axis("off")
+        plt.plot([],[], color = 'white', label = r'$s_{i} = \uparrow$')
+        plt.plot([],[], color = 'black', label = r'$s_{i} = \downarrow$')
         plt.legend(bbox_to_anchor=(1, 1.1))
         
         self.anim = FuncAnimation(self.fig, self.animate2, frames=nframes, interval=(10000.0/nframes))
@@ -106,7 +109,7 @@ def main():
     argp = argparse.ArgumentParser()
     argp.add_argument("-n", default = 10)
     argp.add_argument("-j", default = 1)
-    argp.add_argument("-b", default = 0.4)
+    argp.add_argument("-T", default = 0.4)
     argp.add_argument("-B", default = 1)
     argp.add_argument("-s", default = 10)
     argp.add_argument("-d", default = 0.5)
@@ -114,7 +117,7 @@ def main():
     argp.add_argument("--gridanimation")
     argp.add_argument("--magnetization")
     args = argp.parse_args()
-    model = Ising(int(args.n),float(args.j),float(args.b),float(args.B),int(args.s),float(args.d))
+    model = Ising(int(args.n),float(args.j),float(args.T),float(args.B),int(args.s),float(args.d))
     print("Start")
     model.Simulation(args.stepimage,args.gridanimation)
     print("Koniec")
@@ -123,7 +126,7 @@ def main():
         DF = pd.DataFrame({"t" : t, "M" : m})
         DF.to_csv(f"{args.magnetization}.txt", index = False)
         plt.figure(figsize=(15,10))
-        plt.title(f"Magnetyzacja w funkcji kroku czasowego | B = {args.b}")
+        plt.title(f"Magnetyzacja w funkcji kroku czasowego | B = {args.B}")
         plt.ylabel("M(t)")
         plt.xlabel("t")
         _ = plt.plot(t,m, color = "violet")
